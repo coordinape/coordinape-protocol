70d41b82e653adf9639f60dc5f8b62729f3a9f6e
cesarsld <owlofmoistness@gmail.com>
2021-04-02 01:43:25 +0200

    WIP

--- contracts/Coordinape.sol
+++ contracts/Coordinape.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.2;
 
 library Coordinape {
-    uint8 public constant EXTERNAL = 0; // 000
-    uint8 public constant PARTICIPANT = 1; // 001
-    uint8 public constant RECEIVER = 2; // 010
-    uint8 public constant GIVER = 4; // 100
+    uint8 public constant EXTERNAL = 0;     // 0000
+    uint8 public constant PARTICIPANT = 1;  // 0001
+    uint8 public constant RECEIVER = 2;     // 0010
+    uint8 public constant GIVER = 4;        // 0100
 }

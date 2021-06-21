abbdb3a42d9fe7ae0a76a34aa6620d88d19a874c
cesarsld <owlofmoistness@gmail.com>
2021-06-21 14:46:40 +0200

    Vault fix

--- interfaces/IApeVault.sol
+++ interfaces/IApeVault.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.2;
 
 interface IApeVault {
-	function approveCircle(address _circle) external;
-	function tap(uint256 _tapValue, uint8 _type) external;
+	function tap(uint256 _tapValue, uint256 _slippage, uint8 _type) external;
 }

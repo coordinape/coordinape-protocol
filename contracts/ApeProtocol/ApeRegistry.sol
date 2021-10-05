beaae45eaccaa98bb2cf03c64b05927734675d0f
cesarsld <owlofmoistness@gmail.com>
2021-10-05 15:06:46 +0200

    Add timelock to ape registry

--- contracts/ApeProtocol/ApeRegistry.sol
+++ contracts/ApeProtocol/ApeRegistry.sol
@@ -1,26 +1,26 @@
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./TimeLock.sol";
 
-contract ApeRegistry is Ownable {
+contract ApeRegistry is TimeLock {
 	address public feeRegistry;
 	address public router;
 	address public distributor;
 	address public factory;
 
-	function setFeeRegistry(address _registry) external onlyOwner {
+	function setFeeRegistry(address _registry) external itself {
 		feeRegistry = _registry;
 	}
 
-	function setRouter(address _router) external onlyOwner {
+	function setRouter(address _router) external itself {
 		router = _router;
 	}
 
-	function setDistributor(address _distributor) external onlyOwner {
+	function setDistributor(address _distributor) external itself {
 		distributor = _distributor;
 	}
 
-	function setFactory(address _factory) external onlyOwner {
+	function setFactory(address _factory) external itself {
 		factory = _factory;
 	}
 }

cd58bdaa31d4241c051770a0841f50f2cc302a91
cesarsld <owlofmoistness@gmail.com>
2021-12-14 18:41:45 +0100

    Codebase cleanup

--- tests/test_tap_no_fork.py
+++ tests/test_tap_no_fork.py
@@ -4,11 +4,11 @@ import json
 TAP_BASE = 1
 TAP_PROFIT = 0
 
-def setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, minter):
+def setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, minter):
     set_fee_call = mock_ape_reg.setFeeRegistry.encode_input(mock_ape_fee)
-    set_router_call = mock_ape_reg.setRouter.encode_input(mock_ape_router)
+    set_router_call = mock_ape_reg.setRouter.encode_input(mock_ape_router_beacon)
     set_distro_call = mock_ape_reg.setDistributor.encode_input(mock_ape_distro)
-    set_factory_call = mock_ape_reg.setFactory.encode_input(mock_ape_factory)
+    set_factory_call = mock_ape_reg.setFactory.encode_input(mock_ape_factory_beacon)
     set_treasury_call = mock_ape_reg.setTreasury.encode_input(minter)
     mock_ape_reg.schedule(mock_ape_reg, set_fee_call, '', '', 0, {'from':minter})
     mock_ape_reg.schedule(mock_ape_reg, set_router_call, '', '', 0, {'from':minter})
@@ -21,19 +21,19 @@ def setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router,
     mock_ape_reg.execute(mock_ape_reg, set_factory_call, '', '', 0, {'from':minter})
     mock_ape_reg.execute(mock_ape_reg, set_treasury_call, '', '', 0, {'from':minter})
 
-def test_tap_profit(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, mock_yearn_vault_factories, ApeVaultWrapper, MockVault, minter, MockToken, interface):
-    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, minter)
+def test_tap_profit(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, mock_yearn_vault_factories, ApeVaultWrapperImplementation, MockVault, minter, MockToken, interface):
+    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, minter)
     user = accounts[0]
     amount = 1_000_000_000_000
     #          100_000_000_000
     mock_token = MockToken.deploy('hello', 'h',{'from':user})
     mock_token.mint(amount, {'from':user})
-    mock_token.approve(mock_ape_router, 2 ** 256 -1, {'from':user})
+    mock_token.approve(mock_ape_router_beacon, 2 ** 256 -1, {'from':user})
     new_vault_tx = mock_yearn_vault_factories.createVault(mock_token, 'yvhello', 'yvh',{'from':user})
     vault = MockVault.at(new_vault_tx.new_contracts[0])
-    tx = mock_ape_factory.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
-    ape_vault = ApeVaultWrapper.at(tx.new_contracts[0])
-    mock_ape_router.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
+    tx = mock_ape_factory_beacon.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
+    ape_vault = ApeVaultWrapperImplementation.at(tx.new_contracts[0])
+    mock_ape_router_beacon.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
     assert vault.balanceOf(ape_vault) >= amount
 
     # 10% yield
@@ -55,19 +55,19 @@ def test_tap_profit(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router
     mock_ape_distro.uploadEpochRoot(ape_vault, circle, token, root, share_total // 12, TAP_PROFIT, {'from': admin})
     assert vault.balanceOf(mock_ape_distro) >= (share_total // 12) * 99 // 100
 
-def test_expected_profit_revert(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, mock_yearn_vault_factories, ApeVaultWrapper, MockVault, minter, MockToken, interface):
-    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, minter)
+def test_expected_profit_revert(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, mock_yearn_vault_factories, ApeVaultWrapperImplementation, MockVault, minter, MockToken, interface):
+    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, minter)
     user = accounts[0]
     amount = 1_000_000_000_000
     #          100_000_000_000
     mock_token = MockToken.deploy('hello', 'h',{'from':user})
     mock_token.mint(amount, {'from':user})
-    mock_token.approve(mock_ape_router, 2 ** 256 -1, {'from':user})
+    mock_token.approve(mock_ape_router_beacon, 2 ** 256 -1, {'from':user})
     new_vault_tx = mock_yearn_vault_factories.createVault(mock_token, 'yvhello', 'yvh',{'from':user})
     vault = MockVault.at(new_vault_tx.new_contracts[0])
-    tx = mock_ape_factory.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
-    ape_vault = ApeVaultWrapper.at(tx.new_contracts[0])
-    mock_ape_router.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
+    tx = mock_ape_factory_beacon.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
+    ape_vault = ApeVaultWrapperImplementation.at(tx.new_contracts[0])
+    mock_ape_router_beacon.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
     assert vault.balanceOf(ape_vault) >= amount
 
     # 10% yield
@@ -87,19 +87,19 @@ def test_expected_profit_revert(mock_ape_reg, mock_ape_fee, mock_ape_distro, moc
     print(vault.pricePerShare())
     print(ape_vault.profit())
 
-def test_bad_harvest_reset_value(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, mock_yearn_vault_factories, ApeVaultWrapper, MockVault, minter, MockToken, interface):
-    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router, mock_ape_factory, minter)
+def test_bad_harvest_reset_value(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, mock_yearn_vault_factories, ApeVaultWrapperImplementation, MockVault, minter, MockToken, interface):
+    setup_protocol(mock_ape_reg, mock_ape_fee, mock_ape_distro, mock_ape_router_beacon, mock_ape_factory_beacon, minter)
     user = accounts[0]
     amount = 1_000_000_000_000
     #           20_000_000_000
     mock_token = MockToken.deploy('hello', 'h', {'from':user})
     mock_token.mint(amount, {'from':user})
-    mock_token.approve(mock_ape_router, 2 ** 256 -1, {'from':user})
+    mock_token.approve(mock_ape_router_beacon, 2 ** 256 -1, {'from':user})
     new_vault_tx = mock_yearn_vault_factories.createVault(mock_token, 'yvhello', 'yvh',{'from':user})
     vault = MockVault.at(new_vault_tx.new_contracts[0])
-    tx = mock_ape_factory.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
-    ape_vault = ApeVaultWrapper.at(tx.new_contracts[0])
-    mock_ape_router.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
+    tx = mock_ape_factory_beacon.createApeVault(mock_token, '0x0000000000000000000000000000000000000000', {'from':user})
+    ape_vault = ApeVaultWrapperImplementation.at(tx.new_contracts[0])
+    mock_ape_router_beacon.delegateDeposit(ape_vault, mock_token, amount, {'from':user})
     assert vault.balanceOf(ape_vault) == amount
 
     # 90% bad yield

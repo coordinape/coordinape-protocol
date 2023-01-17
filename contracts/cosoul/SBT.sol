e05890972e0f15972f1110ed2c7ba7521f4460dd
OwlOfMoistness <owl@yieldguild.games>
2023-01-17 14:35:16 +0000

    Add comments

--- contracts/cosoul/SBT.sol
+++ contracts/cosoul/SBT.sol
@@ -16,6 +16,7 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 	mapping(uint256 => uint256) public burnNonces;
 	mapping(address => uint256) public mintNonces;
 	uint256 public counter;
+	// blobs are uint256 storage divided in 8 uint32 slots
 	mapping(uint256 => uint256) public blobs;
 	
 
@@ -24,6 +25,13 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		_;
 	}
 
+	/**
+	 * @notice
+	 * Init function called during proxy setup
+	 * @param __name Name of SBT
+	 * @param __symbol Symbol of SBT
+	 * @param _signer Address that will provide valid signatures
+	 */
 	function init(string memory __name, string memory __symbol, address _signer) external {
 		require(!initiated);
 		initiated = true;
@@ -33,23 +41,48 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		signer = _signer;
 	}
 
+	/**
+	 * @notice
+	 * Set a new signer. Owner gated
+	 * @param _signer New signer
+	 */
 	function setSigner(address _signer) external onlyOwner {
 		signer = _signer;
 	}
 
 
+	/**
+	 * @notice
+	 * Set a addreses capable of updating blob data of SBTs
+	 * @param _caller New signer
+	 * @param _val Boolean to set/unset
+	 */
 	function setCallers(address _caller, bool _val) external onlyOwner {
 		authorisedCallers[_caller] = _val;
 	}
 
+	/**
+	 * @notice
+	 * Getter function to get the value in a specific slot of a given blob
+	 * @param _slot Slot value. Up to 7
+	 * @param _tokenId Token ID from which to get the blob data
+	 */
 	function getSlot(uint8 _slot, uint256 _tokenId) public view returns(uint256 value) {
 		require(_slot < 8);
 
 		uint256 current = blobs[_tokenId];
+		// uint32 mask that is left shifted to fetch correct slot
 		uint256 mask = 0xffffffff << _slot;
 		value = (current & mask) >> _slot;
 	}
 
+	/**
+	 * @notice
+	 * Function to update the value of a slot in a blob
+	 * @param _slot Slot value. Up to 7
+	 * @param _amount Amout to update
+	 * @param _tokenId Token ID from which to update the blob data
+	 */
 	function setSlot(uint256 _slot, uint32 _amount, uint256 _tokenId) external authorised(msg.sender) {
 		require(_slot < 8);
 
@@ -58,6 +91,13 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		blobs[_tokenId] = (current & mask) | (_amount << _slot);
 	}
 
+	/**
+	 * @notice
+	 * Function to increment the value of a slot in a blob by some amount
+	 * @param _slot Slot value. Up to 7
+	 * @param _amount Amout to increment a slot
+	 * @param _tokenId Token ID from which to update the blob data
+	 */
 	function incSlot(uint8 _slot, uint256 _amount, uint256 _tokenId) external authorised(msg.sender) {
 		require(_slot < 8);
 		uint256 value = getSlot(_slot, _tokenId);
@@ -66,6 +106,13 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		blobs[_tokenId] = current + (_amount << _slot);
 	}
 
+	/**
+	 * @notice
+	 * Function to decrement the value of a slot in a blob by some amount
+	 * @param _slot Slot value. Up to 7
+	 * @param _amount Amout to decrement a slot
+	 * @param _tokenId Token ID from which to update the blob data
+	 */
 	function decSlot(uint8 _slot, uint256 _amount, uint256 _tokenId) external authorised(msg.sender) {
 		require(_slot < 8);
 		uint256 value = getSlot(_slot, _tokenId);
@@ -74,6 +121,14 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		blobs[_tokenId] = current - (_amount << _slot);
 	}
 
+	/**
+	 * @notice
+	 * Function to sync blob data of a token from a merkle tree signed by our signer
+	 * @param _data Blob data that will overwrite current data
+	 * @param _tokenId Token ID from which to update blob
+	 * @param _nonce Sync counter used to prevent replays
+	 * @param _signature Signature provided by our signer to validate leaf data
+	 */
 	function syncWithSignature(uint256 _data ,uint256 _tokenId, uint256 _nonce, bytes calldata _signature) external {
 		require(ownerOf(_tokenId) == msg.sender);
 		require(syncNonces[_nonce]++ == _nonce);
@@ -82,23 +137,45 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		blobs[_tokenId] = _data;
 	}
 
-	function overrideTransfer(address from, address to, uint256 tokenId) external authorised(msg.sender) {
-		_transfer(from, to, tokenId);
+	/**
+	 * @notice
+	 * Function to transfer token under approval of the protocol. Gated by authorised addresses
+	 * @param _from Previous token owner
+	 * @param _to New token owner
+	 * @param _tokenId Token to transfer
+	 */
+	function overrideTransfer(address from, address _to, uint256 _tokenId) external authorised(msg.sender) {
+		_transfer(_from, _to, _tokenId);
 	}
 
+	/**
+	 * @notice
+	 * Function to transfer token under approval of the protocol via signature
+	 * @param _from Previous token owner
+	 * @param _to New token owner
+	 * @param _tokenId Token to transfer
+	 * @param _nonce Transfer counter used to prevent replays
+	 * @param _signature Signature provided by our signer to validate transfer
+	 */
 	function overrideTransferWithSignature(
-		address from,
-		address to,
-		uint256 tokenId,
+		address _from,
+		address _to,
+		uint256 _tokenId,
 		uint256 _nonce,
 		bytes calldata _signature) external {
-		require(ownerOf(tokenId) == msg.sender);
-		require(transferNonces[tokenId]++ == _nonce);
-		require(keccak256(abi.encodePacked(tokenId, _nonce)).toEthSignedMessageHash().recover(_signature) == signer, "Sig not valid");
+		require(ownerOf(_tokenId) == msg.sender);
+		require(transferNonces[_tokenId]++ == _nonce);
+		require(keccak256(abi.encodePacked(_tokenId, _nonce)).toEthSignedMessageHash().recover(_signature) == signer, "Sig not valid");
 
-		_transfer(from, to, tokenId);
+		_transfer(_from, _to, _tokenId);
 	}
 
+	/**
+	 * @notice
+	 * Function to mint token via signature to msg.sender
+	 * @param _nonce Mint counter used to prevent replays
+	 * @param _signature Signature provided by our signer to validate mint
+	 */
 	function mintWithSignature(uint256 _nonce, bytes calldata _signature) external {
 		require(balanceOf(msg.sender) == 0);
 		require(mintNonces[msg.sender]++ == _nonce);
@@ -107,13 +184,20 @@ contract CoSoul is ERC721("CoSoul", "Soul"), Ownable {
 		_mint(msg.sender, ++counter);
 	}
 
-	function burnWithSignature( uint256 tokenId, uint256 _nonce, bytes calldata _signature) external {
-		require(ownerOf(tokenId) == msg.sender); // not necessary?
-		require(burnNonces[tokenId]++ == _nonce);
-		require(keccak256(abi.encodePacked(tokenId, _nonce)).toEthSignedMessageHash().recover(_signature) == signer, "Sig not valid");
+	/**
+	 * @notice
+	 * Function to mint token via signature to msg.sender
+	 * @param _tokenId Token ID to be burnt (fiiire)
+	 * @param _nonce Burn counter used to prevent replays
+	 * @param _signature Signature provided by our signer to validate burn
+	 */
+	function burnWithSignature( uint256 _tokenId, uint256 _nonce, bytes calldata _signature) external {
+		require(ownerOf(_tokenId) == msg.sender); // not necessary?
+		require(burnNonces[_tokenId]++ == _nonce);
+		require(keccak256(abi.encodePacked(_tokenId, _nonce)).toEthSignedMessageHash().recover(_signature) == signer, "Sig not valid");
 		
-		blobs[tokenId] = 0;
-		_burn(tokenId);
+		blobs[_tokenId] = 0;
+		_burn(_tokenId);
 	}
 
 	function transferFrom(address from, address to, uint256 tokenId) public override {

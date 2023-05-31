3ce7b58bbd40720df2e9c27089747235156f550c
crabsinger <83605543+crabsinger@users.noreply.github.com>
2023-05-31 10:19:15 -0700

    format soul proxy

--- contracts/cosoul/SoulProxy.sol
+++ contracts/cosoul/SoulProxy.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.8.2;
 
-import"@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract SoulProxy is TransparentUpgradeableProxy {
-	constructor(address _logic, address _admin, bytes memory _data) public  TransparentUpgradeableProxy(_logic, _admin, _data) {}
+    constructor(
+        address _logic,
+        address _admin,
+        bytes memory _data
+    ) public TransparentUpgradeableProxy(_logic, _admin, _data) {}
 }
